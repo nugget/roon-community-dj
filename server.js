35d46d427d224ccecb762e15f9342ed6a2d2874a
David McNett <nugget@macnugget.org>
2020-04-18 20:15:46 -0500

    Minimal server working.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({
-    port: 8080,
+    port: 4242,
     perMessageDeflate: {
         zlibDeflateOptions: {
             // See zlib defaults.
@@ -24,43 +24,20 @@ const wss = new WebSocket.Server({
 });
 
 function log(...args) {
-    var ts = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    var ts = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
     console.log(ts, ...args);
 }
 
 log("Server Started");
 
-function noop() {}
-
-function heartbeat() {
-    log("heartbeat");
-    this.isAlive = true;
-}
-
 wss.on("connection", function connection(ws, req) {
-    ws.isAlive = true;
-    ws.on('pong', heartbeat);
-
-    log("Connection from %s",  req.connection.remoteAddress);
+    log("Connection from ", req.connection.remoteAddress);
     ws.on("message", function incoming(data) {
-        log("message", data);
+        log("Message", req.connection.remoteAddress, data);
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(data);
             }
         });
     });
-});
-
-const interval = setInterval(function ping() {
-      wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
-}, 30000);
-
-wss.on('close', function close() {
-  clearInterval(interval);
 });
